e6094e31a0237849249e84c838c66602d47deea6
FabioBritto <fabio.tritono@gmail.com>
2026-04-21 15:55:57 -0300

    feat: friendly message when there is no data

--- src/app/components/group-view-card/group-view-card.component.ts
+++ src/app/components/group-view-card/group-view-card.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-group-view-card',
   templateUrl: './group-view-card.component.html',
   styleUrls: ['./group-view-card.component.scss']
 })
-export class GroupViewCardComponent {}
+export class GroupViewCardComponent {
+  @Input() hasItems = false;
+
+  @Input() emptyMessage = 'Nenhum grupo por aqui ainda.';
+}
--- src/app/components/upcoming-activities-card/upcoming-activities-card.component.ts
+++ src/app/components/upcoming-activities-card/upcoming-activities-card.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-upcoming-activities-card',
   templateUrl: './upcoming-activities-card.component.html',
   styleUrls: ['./upcoming-activities-card.component.scss']
 })
-export class UpcomingActivitiesCardComponent {}
+export class UpcomingActivitiesCardComponent {
+  @Input() hasItems = false;
+
+  @Input() emptyMessage = 'Nenhuma atividade por aqui ainda.';
+}
