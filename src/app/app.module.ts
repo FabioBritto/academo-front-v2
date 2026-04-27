a2905685c446e0e258770f0185b6856748b55b7c
FabioBritto <fabio.tritono@gmail.com>
2026-04-27 16:40:25 -0300

    feat: created activate account page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { GroupCardComponent } from './components/group-card/group-card.component
 import { PaginationControlsComponent } from './components/pagination-controls/pagination-controls.component';
 import { GroupDetailsModalComponent } from './components/group-details-modal/group-details-modal.component';
 import { SortFiltersComponent } from './components/sort-filters/sort-filters.component';
+import { ActivateAccountPageComponent } from './pages/activate-account-page/activate-account-page.component';
 
 @NgModule({
   declarations: [
@@ -71,7 +72,8 @@ import { SortFiltersComponent } from './components/sort-filters/sort-filters.com
     GroupCardComponent,
     PaginationControlsComponent,
     GroupDetailsModalComponent,
-    SortFiltersComponent
+    SortFiltersComponent,
+    ActivateAccountPageComponent
   ],
   imports: [
     BrowserModule,
